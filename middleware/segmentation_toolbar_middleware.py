33b6b7e515389749834107f8e8963dfe6f5a6e4d
Martin Koistinen <mkoistinen@gmail.com>
2014-06-18 20:42:43 +0300

    Enable overrides and other features in segment toolbar

--- middleware/segmentation_toolbar_middleware.py
+++ middleware/segmentation_toolbar_middleware.py
@@ -2,11 +2,11 @@
 
 import logging
 
-from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
 
 from aldryn_segmentation.segment_pool import segment_pool, SegmentOverride
-from cms.toolbar.items import SubMenu
+from cms.toolbar.items import SubMenu, Break, AjaxItem
 
 logger = logging.getLogger(__name__)
 
@@ -14,38 +14,61 @@ logger = logging.getLogger(__name__)
 class SegmentationToolbarMiddleware(object):
 
     def process_request(self, request):
-        # if hasattr(request, 'toolbar') and request.toolbar.edit_mode:
-        self.create_segmentation_menu(request.toolbar)
+        if hasattr(request, 'toolbar'):
+            self.create_segmentation_menu(request.toolbar, csrf_token=request.COOKIES.get('csrftoken'))
 
 
-    def create_segmentation_menu(self, toolbar):
+    def create_segmentation_menu(self, toolbar, csrf_token):
         '''
         Using the SegmentPool, create a segmentation menu.
         '''
-        segment_menu = toolbar.get_or_create_menu('aldryn-segmentation-menu', _('Segments'))
+
         pool = segment_pool.get_registered_segments()
+
+        # Count the number of current overrides...
+        num_overrides = 0
+        for segment_class in pool.itervalues():
+            for config in segment_class['configurations'].itervalues():
+                override_state = int(config['override'])
+                if override_state != SegmentOverride.NoOverride:
+                    num_overrides += 1
+
+        segment_menu_name = _('Segments (%s)' % num_overrides) if num_overrides else _('Segments')
+        segment_menu = toolbar.get_or_create_menu('aldryn-segmentation-menu', segment_menu_name)
+
         for segment_class in pool:
             segment_name = pool[segment_class]['name']
 
             segment_class_menu = segment_menu.get_or_create_menu(segment_class, segment_name)
 
             for config in pool[segment_class]['configurations']:
-                override_state = pool[segment_class]['configurations'][config]['override']
+                override_state = int(pool[segment_class]['configurations'][config]['override'])
 
-                config_menu = SubMenu(config, None)
+                config_menu = SubMenu(config, csrf_token)
                 segment_class_menu.add_item(config_menu)
 
                 for override_label, override in [(_('Forced Active'), SegmentOverride.ForcedActive), (_('Forced Inactive'), SegmentOverride.ForcedInactive)]:
 
                     config_menu.add_ajax_item(
                         override_label,
-                        action='http://127.0.0.1:8000/',
+                        action=reverse('admin:set_segment_override'),
                         data={
                             'segment_class': segment_class,
                             'segment_config': config,
                             'override': override,
-                            'value': (override == override_state)
+                            'value': (override != override_state),
                         },
-                        active=(override == override_state),
+                        active=bool(override == override_state),
                         on_success=toolbar.REFRESH_PAGE
                     )
+
+        segment_menu.add_item(Break())
+        reset_ajax_item = AjaxItem(
+            _('Reset all segments'),
+            action=reverse('admin:reset_all_segment_overrides'),
+            csrf_token=csrf_token,
+            data={},
+            disabled=bool(num_overrides == 0),
+            on_success=toolbar.REFRESH_PAGE
+        )
+        segment_menu.add_item(reset_ajax_item)
