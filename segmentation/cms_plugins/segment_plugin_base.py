12bbab18685d90430d5ee49994285c064bce8b0a
Martin Koistinen <mkoistinen@gmail.com>
2014-06-21 22:36:37 +0300

    Whitespace

--- segmentation/cms_plugins/segment_plugin_base.py
+++ segmentation/cms_plugins/segment_plugin_base.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
 from django.utils.translation import ugettext_lazy as _
+
 from cms.plugin_base import CMSPluginBase
 
 import logging
